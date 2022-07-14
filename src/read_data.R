9dbd5633b4594ff54eed761e426356c41f6a7bda
James <youngj1@myumanitoba.ca>
2022-07-14 12:11:18 -0500

    fixed data set reading function

--- src/read_data.R
+++ src/read_data.R
@@ -1,21 +1,6 @@
 library(igraph)
 
 
-read.mutag <- function() {
-  lb <- unlist(read.table("data/mutag.label"))
-  names(lb) <- NULL
-  n <- length(lb)
-  G <- vector("list", n)
-  for (i in 1:n) {
-    G[[i]] <- read_graph(paste0("data/mutag/mutag_", i, ".graphml"), format = "graphml")
-    graph_attr(G[[i]], "label") <- lb[i]
-  }
-  G
-}
-
-
-
-# Not sure if this works
 read.dataset <- function(name){
   lb <- unlist(read.table(paste0("data/", name, ".label")))
   names(lb) <- NULL
@@ -24,6 +9,9 @@ read.dataset <- function(name){
   for (i in 1:n) {
     G[[i]] <- read_graph(paste0("data/", name, "/", name, "_", i, ".graphml"), format = "graphml")
     graph_attr(G[[i]], "label") <- lb[i]
+  }
+  for (i in 1:n) {
+    G[[i]] <- delete_vertex_attr(G[[i]], "id")
   }
   G
 }
