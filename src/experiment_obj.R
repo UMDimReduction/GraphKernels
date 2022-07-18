dcab453f77414933cad481ec3a27fb594f86e012
James <youngj1@myumanitoba.ca>
2022-07-18 16:26:57 -0500

    Added comments

--- src/experiment_obj.R
+++ src/experiment_obj.R
@@ -1,10 +1,22 @@
+#-------------------------------------------------------------------------------
+
+
 #===================================================================
-
-
-createExperimentObject <- function(datasetName, kernel, cost, hyperparameter, runs){
+#'  Creates an object comprised of compositions of lists, that holds
+#'  information regarding fitting SVM with a given graph kernel with 
+#'  combinations of costs, and hyperparameters.
+#'  
+#'  @param datasetName name of the data set
+#'  @param kernel string containing key for kernel
+#'  @param numCost the number of costs to tune
+#'  @param numHyperparameter the number of hyperparameters to tune 
+#'  @param runs the number of reptitions of the experiment
+#'  @return experiment object
+#===================================================================
+createExperimentObject <- function(datasetName, kernel, numCost, numHyperparameter, runs){
   
-  cvRun     <- vector(mode = "list", length = length(cost))
-  hyperRun  <- vector(mode = "list", length = length(hyperparameter))
+  cvRun     <- vector(mode = "list", length = numCost)
+  hyperRun  <- vector(mode = "list", length = numHyperparameter)
   repeatRun <- vector(mode = "list", length = runs)
   
   # List structure for each model fitting with CV
@@ -27,119 +39,122 @@ createExperimentObject <- function(datasetName, kernel, cost, hyperparameter, ru
   
   runList <- list("runs" = repeatRun)
   
-  testObj <- append(list("kernel" = kernel, "dataset" = datasetName), runList)
-  class(testObj) <- "experiment"
+  expObj        <- append(list("kernel" = kernel, "dataset" = datasetName), runList)
+  class(expObj) <- "experiment"
   
-  return(testObj)
+  return(expObj)
 }
 
-# ---------------------------------------------------------------------------
-# Accessors and mutators
 
+# ------------------------------------------- Accessors and mutators
 
-#===================================================================
-# Retrieve the cost, hyperparameter, and accuracy of a particular model
-# in an experiment
-# INPUTS:
-# expObject: The experiment object
-# location: numeric vector of the form (run, hyperparameter, cost)
-# Output: 
-# List vector with the information
-#===================================================================
-getModelInfo <- function(expObj, location){
-  run <- location[1]
-  hyp <- location[2]
-  cst <- location[3]
-  
-  info <- list("cost" = getCost(expObj, run, hyp, cst), 
-               "hyperparameter" = getHyperparam(expObj, run, hyp),
-               "accuracy" = (1 - getTrainingError(expObj, run, hyp, cst)))
-  
-  return(info)
-}
+# --------------------- Accessors
+
 
 getHyperparam <- function(expObj, runLoc, hypLoc){
   return(expObj$runs[[runLoc]][[hypLoc]]$hyperparameter)
 }
 
+
 getNumRuns <- function(expObj){
   return(length(expObj$runs))
 }
+
 
 getNumHyperparams <- function(expObj){
   return(length(expObj$runs[[1]]))
 }
 
+
 getNumCost <- function(expObj){
   return(length(expObj$runs[[1]][[1]]$costs))
 }
+
 
 getCost <- function(expObj, runLoc, hypLoc, cstLoc){
   return(expObj$runs[[runLoc]][[hypLoc]]$costs[[cstLoc]]$cost)
 }
 
+
 getCVerror <- function(expObj, runLoc, hypLoc, cstLoc){
   return(expObj$runs[[runLoc]][[hypLoc]]$costs[[cstLoc]]$cv_error)
 }
+
 
 getTrainingError <- function(expObj, runLoc, hypLoc, cstLoc){
   return(expObj$runs[[runLoc]][[hypLoc]]$costs[[cstLoc]]$training_error)
 }
 
+
 getCVtime <- function(expObj, runLoc, hypLoc, cstLoc){
   return(expObj$runs[[runLoc]][[hypLoc]]$costs[[cstLoc]]$cv_time)
 }
+
 
 getSV <- function(expObj, runLoc, hypLoc, cstLoc){
   return(expObj$runs[[runLoc]][[hypLoc]]$costs[[cstLoc]]$support_vectors)
 }
 
+
 getKernelComputeTime <- function(expObj, runLoc, hypLoc){
   return(expObj$runs[[runLoc]][[hypLoc]]$kernel_compute_time)
 }
+
 
 getDataset <- function(expObj){
   return(expObj$dataset)
 }
 
+
 getKernel <- function(expObj){
   return(expObj$kernel)
 }
 
-#---------------------
+
+#--------------------- Mutators
+
 
 setHyperparam <- function(expObj, hyperparam, runLoc, hypLoc){
   expObj$runs[[runLoc]][[hypLoc]]$hyperparameter <- hyperparam
   return(expObj)
 }
 
+
 setCost <- function(expObj, newCost, runLoc, hypLoc, cstLoc){
   expObj$runs[[runLoc]][[hypLoc]]$costs[[cstLoc]]$cost <- newCost
   return(expObj)
 }
+
 
 setCVerror <- function(expObj, newCVerror, runLoc, hypLoc, cstLoc){
   expObj$runs[[runLoc]][[hypLoc]]$costs[[cstLoc]]$cv_error <- newCVerror
   return(expObj)
 }
 
+
 setTrainingError <- function(expObj, newTrainingError, runLoc, hypLoc, cstLoc){
   expObj$runs[[runLoc]][[hypLoc]]$costs[[cstLoc]]$training_error <- newTrainingError
   return(expObj)
 }
+
 
 setCVtime <- function(expObj, newCVtime, runLoc, hypLoc, cstLoc){
   expObj$runs[[runLoc]][[hypLoc]]$costs[[cstLoc]]$cv_time <- newCVtime
   return(expObj)
 }
 
+
 setSV <- function(expObj, newSV, runLoc, hypLoc, cstLoc){
   expObj$runs[[runLoc]][[hypLoc]]$costs[[cstLoc]]$support_vectors <- newSV
   return(expObj)
 }
 
+
 setKernelComputeTime <- function(expObj, newTime, runLoc, hypLoc){
   expObj$runs[[runLoc]][[hypLoc]]$kernel_compute_time <- newTime
   return(expObj)
 }
+
+
+#-------------------------------------------------------------------------------
 
