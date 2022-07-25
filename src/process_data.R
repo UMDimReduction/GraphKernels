87b096328aa3e2e9d5f4cdcdf8578bf4fdd4bcaa
James <youngj1@myumanitoba.ca>
2022-07-25 13:18:12 -0500

    Fixed line that computed the kernel time instead of the CV time when creating the CV box plot

--- src/process_data.R
+++ src/process_data.R
@@ -37,16 +37,17 @@ processFiles <- function(){
     datasets[[currDataSetName]][[length(datasets[[currDataSetName]]) + 1]] <- experiment
   }
   
-  # message("Creating graphs...")
-  # for(ds in 1:length(datasets)){
-  #   createAccuracyBarplot(datasets[[ds]])
-  #   createKernelTimeBoxplot(datasets[[ds]])
-  #   createCVTimeBoxplot(datasets[[ds]])
-  # }
+  message("Creating graphs...")
+  for(ds in 1:length(datasets)){
+    createAccuracyBarplot(datasets[[ds]])
+    createKernelTimeBoxplot(datasets[[ds]])
+    createCVTimeBoxplot(datasets[[ds]])
+  }
   
   message("Creating table")
   createTable(datasets)
 }
+
 
 #===================================================================
 #' Creates a LaTeX table containing information on the best performing
@@ -82,6 +83,191 @@ createTable <- function(datasets){
   writeLines(table, f)
   close(f)
 }
+
+
+#===================================================================
+#' Creates a bar plot that compares the accuracy of the best performing 
+#' graph kernels, averaged across all runs of the experiment. Stores bar
+#' plot in a pdf in the figures directory.
+#' 
+#' @param experimentList list of experiment objects on the same data set
+#===================================================================
+createAccuracyBarplot <- function(experimentList){
+  
+  numExperiments  <- length(experimentList)
+  #col             <- c(rep(0, numExperiments))
+  #accuracyDF      <- data.frame("Kernel" = col, "Accuracy" = col, "sd" = col)
+  accuracyDF      <- data.frame("Kernel" = "", "Accuracy" = 0, "sd" = 0)
+  
+  for(i in 1:numExperiments){
+    
+    currExperiment <- experimentList[[i]]
+    
+    runs <- getNumRuns(currExperiment)
+    accuracy    <- c(rep(0, runs))
+    bestModelLoc <- vector(mode = "list", length = runs)
+    
+    for(currRun in 1:runs){
+      bestModelLoc[[currRun]] <- getBestModel(currExperiment, currRun)
+      
+      r <- bestModelLoc[[currRun]][1]
+      h <- bestModelLoc[[currRun]][2]
+      c <- bestModelLoc[[currRun]][3]
+      
+      accuracy[currRun] <- (1 - getTrainingError(currExperiment, r, h, c))
+    }
+    
+    accuracyDF[i, 1] <- getKernel(currExperiment)
+    accuracyDF[i, 2] <- mean(accuracy) * 100
+    accuracyDF[i, 3] <- sd(accuracy) * 100
+  }
+  
+  pdf(file = paste0("./figures/", getDataset(experimentList[[1]]), "_accuracy.pdf"))
+  print(ggplot(accuracyDF) +
+        ggtitle(paste0(toupper(getDataset(experimentList[[1]])), " accuracy comparison")) +
+        geom_bar(aes(x = Kernel, y = Accuracy), stat = "identity", fill = "deepskyblue4", alpha = 0.5) +
+        geom_errorbar(aes(x = Kernel, ymin = Accuracy - sd, ymax = Accuracy + sd), width = 0.4, colour = "black", alpha = 0.9, size = 1.3) +
+        ylim(0, 100))
+  dev.off()
+
+}
+
+
+#===================================================================
+#' Creates a box plot that compares the kernel computation time across 
+#' all runs of the experiment on a particular data set. Stores box plot
+#' in a pdf in the figures directory.
+#' 
+#' @param experimentList list of experiment objects on the same data set
+#===================================================================
+createKernelTimeBoxplot <- function(experimentList){
+  
+  numExperiments  <- length(experimentList)
+  
+  computeKTimeDF <- data.frame("Kernel" = "", "ComputeTime" = 0)
+  
+  count <- 1
+  for(i in 1:numExperiments){
+    times      <- getKernelTimes(experimentList[[i]])
+    kernelName <- getKernel(experimentList[[i]])
+    
+    for(j in 1:length(times)){
+      computeKTimeDF[count, 1] <- kernelName
+      computeKTimeDF[count, 2] <- times[j] 
+      
+      count <- count + 1
+    }
+  }
+  
+  # Boxplot code for kernel compute time
+  pdf(file = paste0("./figures/", getDataset(experimentList[[1]]), "_KernelComputeTime.pdf"))
+  print(ggplot(computeKTimeDF, aes(x = Kernel, y = ComputeTime)) + 
+          ggtitle(paste0(toupper(getDataset(experimentList[[1]])), " average Gram matrix computation time comparison")) +
+          geom_boxplot(outlier.colour = "black", outlier.shape = 16,
+                       outlier.size = 2, notch = FALSE) +
+          geom_jitter(shape=16, position=position_jitter(0.2)) +
+          xlab("Kernel") +
+          ylab("Computation Time (seconds)") +
+          #ylim(0, NA) +
+          scale_y_continuous(trans='log10'))
+  dev.off()
+}
+
+
+# this doesn't work. different kernels can have different numbers of hyperparameters.
+
+#===================================================================
+#' Creates a boxplot that compares the cross-validation computation 
+#' time across all runs of the experiment on a particular data set.
+#' 
+#' @param experimentList list of experiment objects on the same data set
+#===================================================================
+createCVTimeBoxplot <- function(experimentList){
+
+  numExperiments  <- length(experimentList)
+  
+  computeFTimeDF <- data.frame("Kernel" = "", "ComputeTime" = 0)
+  
+  count <- 1
+  for(i in 1:numExperiments){
+    times      <- getFittingTimes(experimentList[[i]])
+    kernelName <- getKernel(experimentList[[i]])
+    
+    for(j in 1:length(times)){
+      computeFTimeDF[count, 1] <- kernelName
+      computeFTimeDF[count, 2] <- times[j] 
+      count <- count + 1
+    }
+  }
+  
+  # Boxplot code for svm fitting time
+  pdf(file = paste0("./figures/", getDataset(experimentList[[1]]), "_svmFittingTime.pdf"))
+  print(ggplot(computeFTimeDF, aes(x = Kernel, y = ComputeTime)) + 
+        ggtitle(paste0(toupper(getDataset(experimentList[[1]])), " average cross-validation fitting time")) +
+        geom_boxplot(outlier.colour = "black", outlier.shape = 16,
+                       outlier.size = 2, notch = FALSE) +
+        geom_jitter(shape=16, position=position_jitter(0.2)) +
+        xlab("Kernel") +
+        ylab("CV Fitting Time (seconds)") +
+        scale_y_continuous(trans='log10'))
+          
+  dev.off()
+}
+
+
+#===================================================================
+#' Gets all kernel compute times from the given experiment object.
+#' 
+#' @param experiment experiment object
+#' @return numeric vector containing times
+#===================================================================
+getKernelTimes <- function(experiment){
+  
+  numRuns        <- getNumRuns(experiment)
+  numHyperparams <- getNumHyperparams(experiment)
+  
+  kernelTimes <- c(rep(0, numRuns * numHyperparams))
+  
+  count <- 1
+  for(r in 1:numRuns){
+    for(h in 1:numHyperparams){
+      kernelTimes[count] <- getKernelComputeTime(experiment, r, h)
+      count <- count + 1
+    }
+  }
+  
+  return(kernelTimes)
+}
+
+
+#===================================================================
+#' Gets all cross-validation computation times from the given 
+#' experiment object.
+#' 
+#' @param experiment experiment object
+#' @return numeric vector containing times
+#===================================================================
+getFittingTimes <- function(experiment){
+  
+  numRuns        <- getNumRuns(experiment)
+  numHyperparams <- getNumHyperparams(experiment)
+  numCost        <- getNumCost(experiment)
+  
+  fittingTimes <- c(rep(0, numRuns * numHyperparams * numCost))
+  count <- 1
+  
+  for(r in 1:numRuns){
+    for(h in 1:numHyperparams){
+      for(c in 1:numCost){
+        fittingTimes[count] <- getCVtime(experiment, r, h, c)
+        count <- count + 1 
+      }
+    }
+  }
+  
+  return(fittingTimes)
+}
+
 
 #===================================================================
 #' Finds the best graph kernel in terms of cross-validation performance
@@ -124,190 +310,6 @@ getOverallBestKernel <- function(experiments){
   }# for ex
   
   return(location)
-}
-
-
-#===================================================================
-#' Creates a bar plot that compares the accuracy of the best performing 
-#' graph kernels, averaged across all runs of the experiment. Stores bar
-#' plot in a pdf in the figures directory.
-#' 
-#' @param experimentList list of experiment objects on the same data set
-#===================================================================
-createAccuracyBarplot <- function(experimentList){
-  
-  numExperiments  <- length(experimentList)
-  #col             <- c(rep(0, numExperiments))
-  #accuracyDF      <- data.frame("Kernel" = col, "Accuracy" = col, "sd" = col)
-  accuracyDF      <- data.frame("Kernel" = "", "Accuracy" = 0, "sd" = 0)
-  
-  for(i in 1:numExperiments){
-    
-    currExperiment <- experimentList[[i]]
-    
-    runs <- getNumRuns(currExperiment)
-    accuracy    <- c(rep(0, runs))
-    bestModelLoc <- vector(mode = "list", length = runs)
-    
-    
-    for(currRun in 1:runs){
-      bestModelLoc[[currRun]] <- getBestModel(currExperiment, currRun)
-      
-      r <- bestModelLoc[[currRun]][1]
-      h <- bestModelLoc[[currRun]][2]
-      c <- bestModelLoc[[currRun]][3]
-      
-      accuracy[currRun] <- (1 - getTrainingError(currExperiment, r, h, c))
-    }
-    
-    accuracyDF[i, 1] <- getKernel(currExperiment)
-    accuracyDF[i, 2] <- mean(accuracy) * 100
-    accuracyDF[i, 3] <- sd(accuracy) * 100
-  }
-  
-  pdf(file = paste0("./figures/", getDataset(experimentList[[1]]), "_accuracy.pdf"))
-  print(ggplot(accuracyDF) +
-        ggtitle(paste0(toupper(getDataset(experimentList[[1]])), " accuracy comparison")) +
-        geom_bar(aes(x = Kernel, y = Accuracy), stat = "identity", fill = "deepskyblue4", alpha = 0.5) +
-        geom_errorbar(aes(x = Kernel, ymin = Accuracy - sd, ymax = Accuracy + sd), width = 0.4, colour = "black", alpha = 0.9, size = 1.3) +
-        ylim(0, 100))
-  dev.off()
-
-}
-
-
-#===================================================================
-#' Creates a box plot that compares the kernel computation time across 
-#' all runs of the experiment on a particular data set. Stores box plot
-#' in a pdf in the figures directory.
-#' 
-#' @param experimentList list of experiment objects on the same data set
-#===================================================================
-createKernelTimeBoxplot <- function(experimentList){
-  
-  numExperiments  <- length(experimentList)
-  
-  computeKTimeDF <- data.frame("Kernel" = "", "ComputeTime" = 0)
-  count <- 1
-  
-  for(i in 1:numExperiments){
-    times      <- getKernelTimes(experimentList[[i]])
-    kernelName <- getKernel(experimentList[[i]])
-    
-    for(j in 1:length(times)){
-      computeKTimeDF[count, 1] <- kernelName
-      computeKTimeDF[count, 2] <- times[j] 
-      
-      count <- count + 1
-    }
-  }
-  
-  # Boxplot code for kernel compute time
-  pdf(file = paste0("./figures/", getDataset(experimentList[[1]]), "_KernelComputeTime.pdf"))
-  print(ggplot(computeKTimeDF, aes(x = Kernel, y = ComputeTime)) + 
-          ggtitle(paste0(toupper(getDataset(experimentList[[1]])), " average Gram matrix computation time comparison")) +
-          geom_boxplot(outlier.colour = "black", outlier.shape = 16,
-                       outlier.size = 2, notch = FALSE) +
-          geom_jitter(shape=16, position=position_jitter(0.2)) +
-          xlab("Kernel") +
-          ylab("Computation Time (seconds)")+
-          #ylim(0, NA) +
-          scale_y_continuous(trans='log10'))
-  dev.off()
-}
-
-
-# this doesn't work. different kernels can have different numbers of hyperparameters.
-
-#===================================================================
-#' Creates a boxplot that compares the cross-validation computation 
-#' time across all runs of the experiment on a particular data set.
-#' 
-#' @param experimentList list of experiment objects on the same data set
-#===================================================================
-createCVTimeBoxplot <- function(experimentList){
-
-  numExperiments  <- length(experimentList)
-  
-  computeFTimeDF <- data.frame("Kernel" = "", "ComputeTime" = 0)
-  
-  count <- 1
-  
-  for(i in 1:numExperiments){
-    times      <- getKernelTimes(experimentList[[i]])
-    kernelName <- getKernel(experimentList[[i]])
-    
-    for(j in 1:length(times)){
-      computeFTimeDF[count, 1] <- kernelName
-      computeFTimeDF[count, 2] <- times[j] 
-      count <- count + 1
-    }
-  }
-  
-  # Boxplot code for svm fitting time
-  pdf(file = paste0("./figures/", getDataset(experimentList[[1]]), "_svmFittingTime.pdf"))
-  print(ggplot(computeFTimeDF, aes(x = Kernel, y = ComputeTime)) + 
-        ggtitle(paste0(toupper(getDataset(experimentList[[1]])), " average cross-validation computation time")) +
-        geom_boxplot(outlier.colour = "black", outlier.shape = 16,
-                       outlier.size = 2, notch = FALSE) +
-        geom_jitter(shape=16, position=position_jitter(0.2)) +
-        scale_y_continuous(trans='log10'))
-          
-  dev.off()
-}
-
-
-#===================================================================
-#' Gets all kernel compute times from the given experiment object.
-#' 
-#' @param experiment experiment object
-#' @return numeric vector containing times
-#===================================================================
-getKernelTimes <- function(experiment){
-  
-  numRuns        <- getNumRuns(experiment)
-  numHyperparams <- getNumHyperparams(experiment)
-  
-  kernelTimes <- c(rep(0, numRuns * numHyperparams))
-  
-  i <- 1
-  for(r in 1:numRuns){
-    for(h in 1:numHyperparams){
-      kernelTimes[i] <- getKernelComputeTime(experiment, r, h)
-      i <- i + 1
-    }
-  }
-  
-  return(kernelTimes)
-}
-
-
-#===================================================================
-#' Gets all cross-validation computation times from the given 
-#' experiment object.
-#' 
-#' @param experiment experiment object
-#' @return numeric vector containing times
-#===================================================================
-getFittingTimes <- function(experiment){
-  
-  numRuns        <- getNumRuns(experiment)
-  numHyperparams <- getNumHyperparams(experiment)
-  numCost        <- getNumCost(experiment)
-  
-  fittingTimes <- c(rep(0, numRuns * numHyperparams * numCost))
-  i <- 1
-  
-  for(r in 1:numRuns){
-    for(h in 1:numHyperparams){
-      for(c in 1:numCost){
-        fittingTimes[i] <- getCVtime(experiment, r, h, c)
-        i <- i + 1 
-      }
-    }
-  }
-  
-  return(fittingTimes)
 }
 
 
